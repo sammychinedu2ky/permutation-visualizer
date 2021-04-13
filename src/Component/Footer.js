53c3719071335fa447396194f18fcaca75560a78
Samson Amaugo <sammychinedu2ky@gmail.com>
2021-04-13 12:58:50 +0100

    init

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <>
       <Center position="fixed" bottom="0" background="gray.500" p="3" w="100%" >
-        <Text size="xs" textAlign="center" color="white" >
+        <Text as="a" href="https://github.com/sammychinedu2ky/permutation-virtualizer" target="_blank" size="xs" textAlign="center" color="white" >
           ❤️ Swacblooms
           <Image src={github} ml="10" boxSize="7" />
         </Text>
